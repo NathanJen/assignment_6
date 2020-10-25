5a97d5432fd5df4f89c1a4c34352abe09ae01a0b
Nathan Jen <njen@andrew.cmu.edu>
2020-10-25 14:52:41 -0400

    added footer component

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from 'react'
 import './App.css';
 import { Switch, Route } from "react-router-dom";
 import Header from './components/Header/Header'
+import Footer from './components/Footer/Footer'
 import Home from './components/Home/Home'
 
 export default class App extends React.Component {
@@ -12,6 +13,7 @@ export default class App extends React.Component {
         <Switch>
           {/* <Route exact path='/' component={Home} /> */}
         </Switch>
+        <Footer />
       </div>
     )
   }
---
+++ src/components/Footer/Footer.js
@@ -0,0 +1,44 @@
+import React from 'react'
+import styles from './Footer.module.css'
+// import { NavLink } from "react-router-dom"
+import Phone from '../../assets/phone.png'
+import Email from '../../assets/email.png'
+import Facebook from '../../assets/facebook.png'
+import Twitter from '../../assets/twitter.png'
+import Insta from '../../assets/instagram.png'
+
+export default function Footer() {
+  return (
+    <footer>
+      <div className={styles.footerCategory}>
+        <p className={`${styles.footerTitle} green`}>Muddy Paws</p>
+        <div className={styles.row}>
+          <img src={Phone} alt='phone-logo' className={styles.footerIcon} />
+          <p className={styles.footerLink}>412-123-4567</p>
+        </div>
+        <div className={styles.row}>
+          <img src={Email} alt='email-logo' className={styles.footerIcon} />
+          <a href="mailto:info@example.com" className={styles.footerLink}>support@muddypaws.com</a>
+        </div>
+      </div>
+      <div className={styles.footerCategory}>
+        <p className={`${styles.footerTitle} green`}>Products</p>
+        <a href='./dogs.html' className={styles.footerLink}>Cats</a>
+        <a href='./dogs.html' className={styles.footerLink}>Dogs</a>
+      </div>
+      <div className={styles.footerCategory}>
+        <p className={`${styles.footerTitle} green`}>Company</p>
+        <p className={styles.footerLink}>About Us</p>
+        <p className={styles.footerLink}>Contact Us</p>
+      </div>
+      <div className={styles.footerCategory}>
+        <p className={`${styles.footerTitle} green`}>Social Media</p>
+        <div className={styles.socialIconRow}>
+          <a href='https://www.facebook.com/'><img src={Facebook} alt='facebook-logo' /></a>
+          <a href='https://www.twitter.com/'><img src={Twitter} alt='twitter-logo' /></a>
+          <a href='https://www.instagram.com/'><img src={Insta} alt='instagram-logo' /></a>
+        </div>
+      </div>
+    </footer>
+  )
+}
