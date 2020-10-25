8923d07163e03d64806f76f7f269329def157242
Nathan Jen <njen@andrew.cmu.edu>
2020-10-25 12:55:15 -0400

    added header component

---
+++ src/components/Header/Header.js
@@ -0,0 +1,41 @@
+import React from 'react'
+import styles from './Header.module.css'
+import { NavLink } from "react-router-dom"
+import Search from '../../assets/search.png'
+import Favorites from '../../assets/favorite.png'
+import Cart from '../../assets/cart.png'
+import User from '../../assets/user.png'
+
+function Nav() {
+  return (
+    <nav>
+      <ul className={styles.navLinks}>
+        <li><NavLink to='./'><p className={styles.navLink}>Cats</p></NavLink></li>
+        <li><NavLink to='./'><p className={styles.navLink}>Dogs</p></NavLink></li>
+      </ul>
+    </nav>
+  )
+}
+
+function Icons() {
+  return (
+    <div>
+      <ul className={styles.navLinks}>
+        <li><img src={Search} alt='search-icon' className={styles.navLink} /></li>
+        <li><NavLink to='./'><img src={Favorites} alt='favorites-icon' className={styles.navLink} /></NavLink></li>
+        <li><img src={Cart} alt='cart-icon' className={styles.navLink} /></li>
+        <li><img src={User} alt='account-icon' className={styles.navLink} /></li>
+      </ul>
+    </div>
+  )
+}
+
+export default function Header() {
+  return (
+    <header>
+      <NavLink to='./'><h1 className={`${styles.logo} green`}>Muddy Paws</h1></NavLink>
+      <Nav />
+      <Icons />
+    </header>
+  )
+}
